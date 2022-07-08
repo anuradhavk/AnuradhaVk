23b41c9e3847dd6448a4565037f63279d8986342
DELL <DELL@DESKTOP-V3BN9VN>
2022-07-08 14:24:11 +0530

    hiiii

--- Appium36/src/test/java/Programs/CalculatorAdd.java
+++ Appium36/src/test/java/Programs/CalculatorAdd.java
@@ -12,7 +12,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
-//anuradha
+//anuradha roopa
 public class CalculatorAdd {
 	public void  performAddition() throws MalformedURLException, InterruptedException {
 		DesiredCapabilities dc=new DesiredCapabilities();
