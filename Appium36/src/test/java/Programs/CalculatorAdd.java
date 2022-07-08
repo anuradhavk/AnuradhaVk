ad0405cb9d703436f988136ffb28be7673b49d43
anuradhavk <105345140+anuradhavk@users.noreply.github.com>
2022-07-08 14:39:00 +0530

    Update CalculatorAdd.java

--- Appium36/src/test/java/Programs/CalculatorAdd.java
+++ Appium36/src/test/java/Programs/CalculatorAdd.java
@@ -12,7 +12,7 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.touch.TapOptions;
 import io.appium.java_client.touch.offset.ElementOption;
-//anuradha roopa hillllllllllllllllllllllll
+
 //pull request
 public class CalculatorAdd {
 	public void  performAddition() throws MalformedURLException, InterruptedException {
